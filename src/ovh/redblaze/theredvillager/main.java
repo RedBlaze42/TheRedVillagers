b74f572fc9521b486c1c27ee8057bdf5cf29055b
RedBlaze42 <allairehugo@gmail.com>
2019-06-12 20:40:57 +0200

    Villageois
    La commande /villagers display permet de faire spawner tout les types de
    villageois
    La commande /villagers display remove tue tout les villageois créés avec
    la commande ci dessus à un rayon de 100 blocks autour du joueur

--- src/ovh/redblaze/theredvillager/main.java
+++ src/ovh/redblaze/theredvillager/main.java
@@ -1,5 +1,76 @@
 package ovh.redblaze.theredvillager;
 
-public class main {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class main extends JavaPlugin implements Listener {
+	
+	public void onEnable() {
+		saveDefaultConfig();
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(label.equalsIgnoreCase("villagers") && sender.isOp()) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("Vous devez être un joueur");
+				return true;
+			}
+			if(args.length==0) {
+				sender.sendMessage("Mauvaise utilisation");
+				return true;
+			}
+			if(args[0].equalsIgnoreCase("display")) {
+				command_display(sender, args);
+				return true;
+			}
+				
+		}
+		
+		return false;
+	}
+	
+	boolean command_display(CommandSender sender, String[] args) {
+		Player player = (Player) sender;
+		if(args.length==2 && args[1].equalsIgnoreCase("remove")) {
+			List<Entity> entity_list = player.getWorld().getEntities();
+			for (Entity entity : entity_list) {
+				if(entity.getScoreboardTags().contains("display_villager") && entity.getLocation().distance(player.getLocation())<100) {
+					entity.remove();
+				}
+			}
+			player.sendMessage("Villageois de démo retirés");
+			return true;
+		}
+		player.sendMessage("Création des villageois");
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(this.getDataFolder() + "/display_villagers.txt"));
+			String line = reader.readLine();
+			while (line != null) {
+				Bukkit.dispatchCommand(sender, line);
+				line = reader.readLine();
+			}
+			reader.close();
+			player.sendMessage("Villageois créés");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
 
 }
